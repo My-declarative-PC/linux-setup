bd32d70f9ba5e7d9169bbf6530fe142a36e94d49
Timofey Sitnikov <tima.sitnikov@mail.ru>
2024-06-30 06:14:13 -0400

    Dobavil flatpaki dlya ustanivki

--- src/flatpak.sh
+++ src/flatpak.sh
@@ -3,9 +3,15 @@
 source /tmp/debian-setup/common/pretty_print.sh
 pretty_print 'Install *FlatPak*'
 
+echo "Install the latest version FlatPak packages"
 nala install -y flatpak
+
+echo "Add FlatHub repos"
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 echo 'export PATH=$PATH:/var/lib/flatpak/exports/bin' >> /etc/profile
 
+echo "Install esentials flatpaks"
 flatpak install --assumeyes flathub org.mozilla.firefox
+flatpak install --assumeyes flathub org.telegram.desktop
 flatpak install --assumeyes flathub org.wezfurlong.wezterm
+flatpak install --assumeyes flathub org.keepassxc.KeePassXC
