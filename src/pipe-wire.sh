1f494398349bcd562ca17065a10ad47a5f7a8405
Timofey Sitnikov <tima.sitnikov@mail.ru>
2024-06-30 06:16:12 -0400

    Ustanovil paketi dlya upravleniya zvukom

---
+++ src/pipe-wire.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /tmp/debian-setup/common/pretty_print.sh
+pretty_print 'Install *Pipe Wire*'
+
+echo "Install the latest version Pipe Wire packages"
+nala install -y \
+     pipewire   \
+     pipewire-pulse
+
+echo "enable Pipe Wire"
+systemctl --now enable pipewire pipewire-pulse
