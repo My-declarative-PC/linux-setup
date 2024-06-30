4949b1abeaf0584a0a9c7570a5692b0b791789d4
Timofey Sitnikov <tima.sitnikov@mail.ru>
2024-06-30 06:15:42 -0400

    Ustanovil paketi dlya raboti s set'yu

---
+++ src/network-manager.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /tmp/debian-setup/common/pretty_print.sh
+pretty_print 'Install *Network Manager*'
+
+echo "Install the latest version Network Manager packages"
+nala install -y              \
+     network-manager-ssh     \
+     network-manager-openvpn \
+     network-manager
+
+echo "enable Network Manager"
+systemctl enable --now NetworkManager
