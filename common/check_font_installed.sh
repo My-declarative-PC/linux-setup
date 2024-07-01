2b2de46c136198c152227e362f4e0d30be964e79
Timofey Sitnikov <tima.sitnikov@mail.ru>
2024-07-01 23:11:05 +0300

    smenil proveryaemuyu directoriyu s shriftami

--- common/check_font_installed.sh
+++ common/check_font_installed.sh
@@ -3,7 +3,7 @@
 # Function to check if font directory exists
 check_font_installed() {
     font_name=$1
-    if [ -d ~/.local/share/fonts/$font_name ]; then
+    if [ -d /ust/share/fonts/$font_name ]; then
         echo "Font $font_name is already installed. Skipping."
         return 0  # Font already installed
     else
