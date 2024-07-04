e5519d78ee4c67a377eeb540f65dc497a869ffd2
Timofey Sitnikov <t.sitnikov@infocom-s.ru>
2024-07-04 12:13:48 +0300

    Скорректировал скрипт установки докера

--- src/docker.sh
+++ src/docker.sh
@@ -6,17 +6,20 @@ print_installer_start 'Docker'
 echo "Needed for docker installer"
 nala install -y ca-certificates curl
 
+echo "Delete old versions"
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo nala remove $pkg; done
+
 echo "Add Docker's official GPG key"
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 echo "Add the repository to Apt sources"
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
-nala update
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo nala update
 
 echo "Install the latest version Docker packages"
 nala install -y           \
