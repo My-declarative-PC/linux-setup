835d898a053543edeeb099d51188a6ae3ff91edf
Timofey Sitnikov <t.sitnikov@infocom-s.ru>
2024-07-05 12:03:28 +0300

    Переименовл директорию с установочными скриптами

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Run installers"
-for script in /var/lib/debian-setup/src/*.sh; do source $script; done
+for script in /var/lib/debian-setup/installers/*.sh; do source $script; done
