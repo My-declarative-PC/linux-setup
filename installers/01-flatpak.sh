1a03c6538239680d8228a7e2bea00e3466794967
Timofey Sitnikov <t.sitnikov@infocom-s.ru>
2024-08-22 17:11:10 +0300

    Добавил ветку в которой будут храниться общие для всех DE

---
+++ installers/01-flatpak.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source /var/lib/linux-setup/common/print_installer_start.sh
+PACK='FlatPak'
+print_installer_start $PACK
+
+print_installer_step "Install the latest version ${PACK} packages"
+sudo nala install -y flatpak
+
+print_installer_step "Add FlatHub repos"
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo echo 'export PATH=$PATH:/var/lib/flatpak/exports/bin' >> /etc/profile
+
+print_installer_step "Install esentials flatpaks"
+flatpak install --assumeyes flathub org.mozilla.firefox
+sudo nala remove -y firefox
+
+flatpak install --assumeyes flathub org.telegram.desktop
+
+flatpak install --assumeyes flathub org.wezfurlong.wezterm
+
+flatpak install --assumeyes flathub org.keepassxc.KeePassXC
+
+print_installer_stop $PACK
