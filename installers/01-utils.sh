8ba504bea8b831254457e1dbdb7b04900841a7c0
Sitnikov Timofey <tima.sitnikov@mail.ru>
2024-08-24 21:01:43 +0300

    Добавил пакеты для работы блокировки с эффектом размытия

--- installers/01-utils.sh
+++ installers/01-utils.sh
@@ -4,15 +4,17 @@ source /var/lib/linux-setup/common/print_installer_start.sh
 PACK='Some smale Utils'
 print_installer_start $PACK
 
-sudo nala install -y \
-     btop            \
-     btrfs-compsize  \
-     distrobox       \
-     eza             \
-     fish            \
-     htop            \
-     vim             \
-     zoxide          \
+sudo nala install -y   \
+     btop              \
+     btrfs-compsize    \
+     distrobox         \
+     eza               \
+     fish              \
+     htop              \
+     imagemagick-6.q16 \
+     scrot             \
+     vim               \
+     zoxide            \
      zram-tools
 
 print_installer_stop $PACK
