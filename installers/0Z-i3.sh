78db008dee7aaccd7f5917dec97c028a5222c9d6
ImperatorMarsa <tima.sitnikov@mail.ru>
2024-08-24 07:23:35 +0300

    добавил установку необходимой зависимости

--- installers/0Z-i3.sh
+++ installers/0Z-i3.sh
@@ -8,6 +8,7 @@ print_installer_step "Install the latest version ${PACK} packages"
 sudo nala install -y \
      i3              \
      feh             \
-     picom
+     picom           \
+     i3blocks
 
 print_installer_stop $PACK
