4575ae34d1501900d9602e713fe43d28809c1870
Timofey Sitnikov <t.sitnikov@infocom-s.ru>
2024-08-28 13:22:40 +0300

    Исправил установку темы
    добавил переход во временную директорию

--- installers/Z1-set_theme.sh
+++ installers/Z1-set_theme.sh
@@ -3,6 +3,8 @@
 source /var/lib/linux-setup/common/print_installer_start.sh
 PACK="CatPuccin Theme"
 print_installert_start $PACK
+mkdir -p /tmp/theme
+cd /tmp/theme
 
 curl -LsSO "https://raw.githubusercontent.com/catppuccin/gtk/v1.0.3/install.py"
 python3 install.py mocha mauve
@@ -13,4 +15,5 @@ export ACCENT="mauve"
 
 sudo flatpak override --env=GTK_THEME="catppuccin-${FLAVOR}-${ACCENT}-standard+default"
 
+rm -rf /tmp/theme
 print_installer_stop $PACK
