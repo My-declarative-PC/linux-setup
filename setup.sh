601d2839ccf9939ae6755c441397d27eaeb63c43
ImperatorMarsa <tima.sitnikov@mail.ru>
2024-10-04 10:37:23 +0300

    Изменил струкруту и последовательност установлеваемых пакетов

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+echo "Needed for all installers"
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y nala
+
+sudo nala install -y git sudo apt-utils sd jq curl
+
 echo "Run installers"
 for script in /var/lib/linux-setup/installers/*.sh; do source $script; done
